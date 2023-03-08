fc268bab3c92e7da5d7c6d859d904ee837ed862c
topoescobar <ivan90ge@gmail.com>
2023-03-08 11:54:10 -0300

    script de clasificacion de eventos pasados y futuros

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+const divAlbum = document.getElementById('album');
+
+    for (activity of data.events) {
+        const card = document.createElement('div');
+        card.classList.add('col');
+        card.innerHTML = `
+        <div class="card shadow-sm">
+          <img src="${activity.image}" alt="event" class="cards_img object-fit-cover border rounded">
+          <div class="card-body">
+            <h3>${activity.name}</h3>
+            <p class="card-text ">${activity.description} </p>
+            <div class="d-flex justify-content-between align-items-center">
+              <small class="text-muted">Price: $$$</small>
+              <div class="btn-group">
+                <button type="button" class="btn btn-dark">View</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      `
+      divAlbum.append(card);
+    }
