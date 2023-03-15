6971b5c7dda07f89c77d01236ca722f127713c60
topoescobar <ivan90ge@gmail.com>
2023-03-15 00:20:07 -0300

    categorias checkbox

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
 const divAlbum = document.getElementById('album');
 
-    for (activity of data.events) {
-        const card = document.createElement('div');
-        card.classList.add('col', 'card_col');
-        card.innerHTML = `
+function displayAlbum() {
+  for (activity of data.events) {
+    const card = document.createElement('div');
+    card.classList.add('col', 'card_col');
+    card.innerHTML = `
         <div class="card border rounded shadow-sm">
           <img src="${activity.image}" alt="event" class="cards_img object-fit-cover">
           <div class="card-body">
@@ -18,31 +19,48 @@ const divAlbum = document.getElementById('album');
           </div>
         </div>
       `
-      divAlbum.append(card);
+    divAlbum.append(card);
+  }
+}
+
+// displayAlbum();
+
+function searching() {
+  let cards = document.querySelectorAll('.card_col')
+  let searchInput = document.getElementById("search").value
+  for (var i = 0; i < cards.length; i++) {
+    if (cards[i].innerText.toLowerCase().includes(searchInput.toLowerCase())) {
+      cards[i].classList.remove("is_hidden");
+    } else {
+      cards[i].classList.add("is_hidden");
     }
+  }
+}
 
-  
-
-    // console.log(searchCaptured);
-    
-    function searching() {
-      let cards = document.querySelectorAll('.card_col')
-      let searchInput = document.getElementById("search").value
-      for (var i = 0; i < cards.length; i++) {
-        if(cards[i].innerText.toLowerCase().includes(searchInput.toLowerCase())) {
-            cards[i].classList.remove("is_hidden");
-        } else {
-          cards[i].classList.add("is_hidden");
-        }
-      }
-    }
-
-//delay on search
+const arrayEvents = data.events
+const renderList = (arrayEvents) => {
+  divAlbum.innerHTML = '';
+  arrayEvents.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = item.city;
+    displayDiv.append(listItem);
+  })
+}
 
 
-/* 
-searchCaptured.addEventListener("input", (e)=>{
-  console.log(e.target.value);
+let choices = [];
+const categoryList = document.querySelectorAll('.category');
+console.log(categoryList)
+
+
+categoryList.forEach(category => {
+  category.addEventListener('change', () => {
+    category.checked ? 
+      choices.push(category.value)
+      : choices.splice( choices.indexOf(category.value), 1 );
+    console.log(choices)
+    const filteredArray = arrayEvents.filter(item => choices.includes(item.category));
+    console.log(filteredArray)
+    renderList(filteredArray)
+  })
 })
-
- */
