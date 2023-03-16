de0a1485eff5b4a2ffc7863b071f2368e69efa03
topoescobar <ivan90ge@gmail.com>
2023-03-16 03:02:56 -0300

    filtros cruzados funcionando

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-const divAlbum = document.getElementById('album');
+/*const divAlbum = document.getElementById('album');
 
-function displayAlbum() {
+ function displayAlbum() {
   for (activity of data.events) {
     const card = document.createElement('div');
     card.classList.add('col', 'card_col');
@@ -23,9 +23,9 @@ function displayAlbum() {
   }
 }
 
-// displayAlbum();
+displayAlbum();
 
-function searching() {
+ function searching() {
   let cards = document.querySelectorAll('.card_col')
   let searchInput = document.getElementById("search").value
   for (var i = 0; i < cards.length; i++) {
@@ -35,32 +35,95 @@ function searching() {
       cards[i].classList.add("is_hidden");
     }
   }
+} 
+ */
+
+const searchInput = document.querySelector("input[type='search']")
+const checkContainer = document.getElementById('checkContainer')
+const album = document.getElementById('album')
+
+searchInput.addEventListener('input', crossFilter)
+checkContainer.addEventListener('change', crossFilter)
+
+renderCards(data.events)
+renderCheckBoxes(data.events)
+
+function crossFilter() {
+  let inputFilter = inputFiltering(data.events, searchInput.value)
+  let categoryFilter = categoryFiltering(inputFilter)
+  renderCards(categoryFilter)
 }
 
-const arrayEvents = data.events
-const renderList = (arrayEvents) => {
-  divAlbum.innerHTML = '';
-  arrayEvents.forEach(item => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = item.city;
-    displayDiv.append(listItem);
+function renderCheckBoxes(array) {
+  //crea el array incluyendo categorias repetidas
+  let eventsArray = array.map(event => event.category)
+  // console.log(eventsArray)
+
+  //con set se almacenan valores unicos
+  let setEvents = new Set(eventsArray)
+  // console.log(setEvents)
+
+  //transformar en array
+  let arrayChecks = Array.from(setEvents)
+  // console.log(arrayChecks)
+  let checkboxes = ''
+  arrayChecks.forEach(category => {
+    checkboxes += `<label class="px-1">
+                      <input type="checkbox" name="category" value="${category}" class="category"> 
+                      ${category} 
+                    </label>`
   })
+  checkContainer.innerHTML = checkboxes
 }
 
 
-let choices = [];
-const categoryList = document.querySelectorAll('.category');
-console.log(categoryList)
-
-
-categoryList.forEach(category => {
-  category.addEventListener('change', () => {
-    category.checked ? 
-      choices.push(category.value)
-      : choices.splice( choices.indexOf(category.value), 1 );
-    console.log(choices)
-    const filteredArray = arrayEvents.filter(item => choices.includes(item.category));
-    console.log(filteredArray)
-    renderList(filteredArray)
+function renderCards(array) {
+  if (array.length == 0) {
+    album.innerHTML = `<h3 class="display-1 fw-bolder"> Nothing Found </h2>`
+    return
+  }
+  let cards = ''
+  array.forEach(event => {
+    cards += `
+     <div class="col card_col">
+        <div class="card border rounded shadow-sm">
+          <img src="${event.image}" alt="event" class="cards_img object-fit-cover">
+          <div class="card-body">
+            <h3>${event.name}</h3>
+            <p class="card-text ">${event.description} </p>
+            <div class="d-flex justify-content-between align-items-center mt-auto">
+              <small class="text-muted">Price: ${event.price} $</small>
+              <div class="btn-group">
+                <a href="./details.html" class="btn btn-dark">Details</a>
+              </div>
+            </div>
+          </div>
+        </div>
+     </div>
+      `
   })
-})
+  album.innerHTML = cards
+}
+
+function inputFiltering(array, text) {
+  let filteredArray = array.filter(event =>
+    event.name.toLowerCase().includes(text.toLowerCase()))
+  return filteredArray
+}
+
+function categoryFiltering(array) {
+  let checkboxes = document.querySelectorAll("input[type='checkbox']")
+  // console.log(checkboxes); 
+  let arrayChecks = Array.from(checkboxes)
+  // console.log(arrayChecks);
+  let arrayChecksChecked = arrayChecks.filter(check => check.checked)
+  // console.log(arrayChecksChecked);
+  let arrayChecksCheckedValues = arrayChecksChecked.map(checkChecked => checkChecked.value)
+  console.log(arrayChecksCheckedValues);
+  let filteredArray = array.filter(event => arrayChecksCheckedValues.includes(event.category))
+  // console.log(filteredArray);
+  if (arrayChecksChecked.length > 0) {
+    return filteredArray
+  }
+  return array
+}
--- scripts/past.js
+++ scripts/past.js
@@ -1,27 +1,92 @@
-const divAlbum = document.getElementById('album');
-let cardsList = ''
+const album = document.getElementById('album');
+const searchInput = document.querySelector("input[type='search']")
+const checkContainer = document.getElementById('checkContainer')
 
-for (activity of data.events)  {
-    const card = document.createElement('div');
-    card.classList.add('col');
+searchInput.addEventListener('input', crossFilter)
+checkContainer.addEventListener('change', crossFilter)
 
-    if(activity.date < data.currentDate) {
-        cardsList+= `
-        <div class="card shadow-sm">
-          <img src="${activity.image}" alt="event" class="cards_img object-fit-cover border rounded">
-          <div class="card-body">
-            <h3>${activity.name}</h3>
-            <p class="card-text ">${activity.description} </p>
-            <div class="d-flex justify-content-between align-items-center">
-              <small class="text-muted">Price: $$$</small>
-              <div class="btn-group">
-                <a href="./details.html" class="btn btn-dark">Details</a>
-              </div>
+renderPastCards(data.events)
+renderCheckBoxes(data.events)
+
+function crossFilter() {
+  let inputFilter = inputFiltering(data.events, searchInput.value)
+  let categoryFilter = categoryFiltering(inputFilter)
+  renderPastCards(categoryFilter)
+}
+
+function renderPastCards(array) {
+  if (array.length == 0) {
+    album.innerHTML = `<h3 class="display-1 fw-bolder"> Nothing Found </h2>`
+    return
+  }
+  let cardsList = ''
+  array.forEach(aEvent => {
+    if (aEvent.date <= data.currentDate) {
+      cardsList += `
+      <div class="col card_col">
+      <div class="card border rounded shadow-sm">
+        <img src="${aEvent.image}" alt="event" class="cards_img object-fit-cover">
+        <div class="card-body">
+          <h3>${aEvent.name}</h3>
+          <p class="card-text ">${aEvent.description} </p>
+          <div class="d-flex justify-content-between align-items-center mt-auto">
+            <small class="text-muted">Price: ${aEvent.price} $</small>
+            <div class="btn-group">
+              <a href="./details.html" class="btn btn-dark">Details</a>
             </div>
           </div>
         </div>
-      `
+      </div>
+   </div>
+    `
     }
+  });
+  album.innerHTML = cardsList
 }
 
-divAlbum.innerHTML = cardsList
+
+function renderCheckBoxes(array) {
+  //crea el array incluyendo categorias repetidas
+  let eventsArray = array.map(event => event.category)
+  // console.log(eventsArray)
+
+  //con set se almacenan valores unicos
+  let setEvents = new Set(eventsArray)
+  // console.log(setEvents)
+
+  //transformar en array
+  let arrayChecks = Array.from(setEvents)
+  // console.log(arrayChecks)
+  let checkboxes = ''
+  arrayChecks.forEach(category => {
+    checkboxes += `<label class="px-1">
+                      <input type="checkbox" name="category" value="${category}" class="category"> 
+                      ${category} 
+                    </label>`
+  })
+  checkContainer.innerHTML = checkboxes
+}
+
+
+function inputFiltering(array, text) {
+  let filteredArray = array.filter(event =>
+    event.name.toLowerCase().includes(text.toLowerCase()))
+  return filteredArray
+}
+
+function categoryFiltering(array) {
+  let checkboxes = document.querySelectorAll("input[type='checkbox']")
+  // console.log(checkboxes); 
+  let arrayChecks = Array.from(checkboxes)
+  // console.log(arrayChecks);
+  let arrayChecksChecked = arrayChecks.filter(check => check.checked)
+  // console.log(arrayChecksChecked);
+  let arrayChecksCheckedValues = arrayChecksChecked.map(checkChecked => checkChecked.value)
+  console.log(arrayChecksCheckedValues);
+  let filteredArray = array.filter(event => arrayChecksCheckedValues.includes(event.category))
+  // console.log(filteredArray);
+  if (arrayChecksChecked.length > 0) {
+    return filteredArray
+  }
+  return array
+}
