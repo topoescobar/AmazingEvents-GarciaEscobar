e318fefbe36e4aae5c2988cae9097af2a4873ad2
topoescobar <ivan90ge@gmail.com>
2023-03-25 23:15:42 -0300

    tabla funcionando

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,11 +1,7 @@
 const API_URL = "https://mindhub-xj03.onrender.com/api/amazing"
-// let eventsArray 
-// let pastEvents
-// let upcomingEvents
-// let currentDate
+// const API_URL = "./scripts/amazing.json"
 
 async function getData() {
-
     try{
       const response = await fetch(API_URL)
       const data = await response.json()
@@ -13,11 +9,20 @@ async function getData() {
       const eventsArray = data.events
       const currentDate = data.currentDate
 
-      const pastEvents = eventsArray.filter(event => event.date < currentDate)
-      console.log("complete array",eventsArray)
       const upcomingEvents = eventsArray.filter(event => event.date >= currentDate)
+      console.log("upcoming events", upcomingEvents);
+      const pastEvents = eventsArray.filter(event => event.date < currentDate)
+      console.log("past events", pastEvents);
+      const upcomingTable = document.getElementById("table_upcoming")
+      const pastTable = document.getElementById("table_past")
+      
+      let categoryUpcom = statsByCategory(upcomingEvents)
+      let categoryPast = statsByCategory(pastEvents)
 
-      renderEventsStats(pastEvents) 
+      renderGeneralStats(pastEvents)
+      renderTable(categoryUpcom, upcomingTable)
+      renderTable(categoryPast, pastTable)
+
     }
     catch(error){
       console.log(error.message)
@@ -27,6 +32,7 @@ async function getData() {
 getData()
 
 function calcAttendancePercent(array){
+  // console.log("array calc", array);
   let highestPercentage = 0
   let highestPercentageId
   let lowestPercentage = 100
@@ -34,11 +40,9 @@ function calcAttendancePercent(array){
   let highestCapacity = 0
   let highestCapacityId
   array.forEach(event => {
-    if (event.assistance == undefined) {
-      event.attendancePercentage = (event.estimate / event.capacity)*100
-    } else {
-      event.attendancePercentage = (event.assistance / event.capacity)*100
-    } if (event.attendancePercentage > highestPercentage) {
+    let assistanceStandard = event.assistance == undefined ? event.estimate : event.assistance
+    event.attendancePercentage = (assistanceStandard / event.capacity)*100
+     if (event.attendancePercentage > highestPercentage) {
       highestPercentage = event.attendancePercentage
       highestPercentageId = event._id
     } if (event.attendancePercentage < lowestPercentage) {
@@ -48,7 +52,9 @@ function calcAttendancePercent(array){
       highestCapacity = event.capacity
       highestCapacityId = event._id
     }
-    array.push(event.attendancePercentage)
+    // array.push(event.attendancePercentage)
+    console.log("event", event);
+    console.log("att perc", event.attendancePercentage);
   })
   return [highestPercentageId, lowestPercentageId, highestCapacityId]
  }
@@ -83,9 +89,9 @@ function calcAttendancePercent(array){
     return capacityById
   }
 
-  function renderEventsStats(array){
+  function renderGeneralStats(array){
     const [highestPercentageId, lowestPercentageId, highestCapacityId] = calcAttendancePercent(array) //PORQUE CALCULA Y PUSHEA attendancePercentage PARA TODOS LOS EVENTOS SI LE PASO EL ARRAY FILTRADO pastEvents? 
-    console.log("array pastevents de renderEventsStats", array) // aca se ve el array filtrado con attendancePercentage incluso donde no hay objetos
+    // console.log("array pastevents de renderGeneralStats", array) // aca se ve el array filtrado con attendancePercentage incluso donde no hay objetos
     let highestPercent = getAttendancePercent(highestPercentageId,array)
     let highestName = getEventName(highestPercentageId, array)
     let lowestPercent = getAttendancePercent(lowestPercentageId, array)
@@ -96,43 +102,87 @@ function calcAttendancePercent(array){
     const tableSummary = document.getElementById("table_Summary")
     tableSummary.innerHTML = `
     <tr>
-      <td> ${highestName}: ${highestPercent}% </td>
+      <td> ${highestName}: ${highestPercent.toFixed(2)}% </td>
       <td> ${lowestName}: ${lowestPercent}% </td>
       <td> ${lagerCapacityName}: ${largerCapacity} people </td>
     </tr>
     `
   }
 
-
-
-
-
-
-
-function calculateIncome(array){
-  let events = array
-    const initialValue = 0;
-    const totalIncome = events.reduce(
-      (accumulator, currentValue) => {
-        priceEvent = currentValue.price
-        console.log("price: ", priceEvent)
-        assistanceEvent = currentValue.assistance
-        console.log("asistencia: ", assistanceEvent);
-        estimatedAssistance = currentValue.estimate
-        console.log("asisitencia estimada", estimatedAssistance);
-    
-        if (assistanceEvent == undefined){
-          eventIncome = priceEvent * estimatedAssistance
-        }else {
-          eventIncome = priceEvent * assistanceEvent
-        }
-    
-        console.log("ingreso de evento: ",currentValue.name ,eventIncome);
-    
-        return accumulator = accumulator + eventIncome
-      },
-      initialValue
-    )
-    console.log('Ingreso total: ', totalIncome);
+function statsByCategory(array) {
+  //array of existent categories
+  let categories = Array.from (new Set(array.map((arr) => arr.category )))
+  //array of objects grouped by category
+  let eventCategories = categories.map(categ => array.filter(event=> event.category == categ))
+  // console.log("categories", categories);
+  // console.log("eventCategories", eventCategories);
+  let result = eventCategories.map(eventCat => {
+    let calculate = eventCat.reduce((acc, event) => {
+      let assistanceStandard = event.assistance == undefined ? event.estimate : event.assistance
+      // console.log("event:",event)
+      acc.categoryName = event.category; //reemplaza no acumula
+      // console.log("acc.category", acc.category);
+      acc.revenues += event.price * assistanceStandard
+      // console.log("acc.revenues",acc.revenues);
+      acc.attendance += assistanceStandard * 100 / event.capacity
+      // console.log("acc.attendance", acc.attendance);
+      // console.log("att percent", event.attendancePercentage);
+      return acc
+    }, {
+      categoryName: "",
+      revenues: 0,
+      attendance: 0
+    })
+    calculate.attendance = calculate.attendance / eventCat.length
+    // console.log("calculate", calculate)
+    return calculate
+  })
+  // Array de categorias
+  // console.log("result",result) 
+  return result 
 }
+
+function renderTable(array,tableName) {
+  let tableRows = ""
+  array.forEach(category=>{
+    tableRows += `
+    <tr>
+      <td> ${category.categoryName} </td>
+      <td> $ ${category.revenues} </td>
+      <td> ${category.attendance.toFixed(2)} % </td>
+    </tr>
+    `
+  })
+  tableName.innerHTML = tableRows
+}
+
+
+
+
+// function calculateIncome(array){
+//   let events = array
+//     const initialValue = 0;
+//     const totalIncome = events.reduce(
+//       (accumulator, currentValue) => {
+//         priceEvent = currentValue.price
+//         console.log("price: ", priceEvent)
+//         assistanceEvent = currentValue.assistance
+//         console.log("asistencia: ", assistanceEvent);
+//         estimatedAssistance = currentValue.estimate
+//         console.log("asisitencia estimada", estimatedAssistance);
+    
+//         if (assistanceEvent == undefined){
+//           eventIncome = priceEvent * estimatedAssistance
+//         }else {
+//           eventIncome = priceEvent * assistanceEvent
+//         }
+    
+//         console.log("ingreso de evento: ",currentValue.name ,eventIncome);
+    
+//         return accumulator = accumulator + eventIncome
+//       },
+//       initialValue
+//     )
+//     console.log('Ingreso total: ', totalIncome);
+// }
 
